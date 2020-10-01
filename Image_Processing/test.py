c475f2c01eda0cb611c34e554cc4e13712cc67c1
MattYoon <57797966+MattYoon@users.noreply.github.com>
2020-10-01 22:19:34 +0900

    Delete test.py

--- Image_Processing/test.py
+++
@@ -1,8 +0,0 @@
-from Image_Processing import image_processing
-import time
-
-# while True:
-#     time.sleep(2)
-#     print("Speed: ", image_processing.getSpeed())
-#     print("Origin: ", image_processing.getOrigin())
-
